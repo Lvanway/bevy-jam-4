0524905e2d6ddfb05a2394014a36f6f38058bdd7
Marcus Hughes <hughes.jmb@gmail.com>
2023-12-02 12:03:28 -0700

    adds player movement

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,20 @@
 
 use bevy::prelude::*;
 
+const LEFT_WALL: f32 = -450.;
+const RIGHT_WALL: f32 = 450.;
+const BOTTOM_WALL: f32 = -300.;
+const TOP_WALL: f32 = 300.;
+
+const PLAYER_SIZE: Vec3 = Vec3::new(20.0, 20.0, 0.0);
+const PLAYER_SPEED: f32 = 500.0;
+const PLAYER_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, move_player)
         .run();
 }
 
@@ -19,4 +29,66 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture: asset_server.load("icon.png"),
         ..Default::default()
     });
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: PLAYER_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: PLAYER_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Player,
+    ));
+}
+
+#[derive(Component)]
+struct Player;
+
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    let mut player_transform = query.single_mut();
+    let mut direction_x = 0.0;
+    let mut direction_y = 0.0;
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        direction_x -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        direction_x += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Down) {
+        direction_y -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Up) {
+        direction_y += 1.0;
+    }
+
+    // Calculate the new position based on player input
+    let new_player_position_x =
+        player_transform.translation.x + direction_x * PLAYER_SPEED * time.delta_seconds();
+    let new_player_position_y =
+        player_transform.translation.y + direction_y * PLAYER_SPEED * time.delta_seconds();
+
+    // // Update the paddle position,
+    // // making sure it doesn't cause the paddle to leave the arena
+    let left_bound = LEFT_WALL + PLAYER_SIZE.x / 2.0;
+    let right_bound = RIGHT_WALL - PLAYER_SIZE.x / 2.0;
+    let top_bound = TOP_WALL - PLAYER_SIZE.x / 2.0;
+    let bottom_bound = BOTTOM_WALL + PLAYER_SIZE.x / 2.0;
+
+    player_transform.translation.x = new_player_position_x.clamp(left_bound, right_bound);
+    player_transform.translation.y = new_player_position_y.clamp(bottom_bound, top_bound);
+
 }
